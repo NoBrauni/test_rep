76b105f06c83d797b660de0b3df5bd8caa5021d6
Strawberry17 <45404454+NoBrauni@users.noreply.github.com>
2022-05-11 13:53:44 +0200

    Add rating system

--- Another file.py
+++ Another file.py
@@ -1,7 +1,15 @@
 import random
 
-#This is another file
+# This is another file
+k = random.randint(1, 200)
 
-x = random.randint(1, 15)
 
-print(x)
+def rate_number(x):
+    if x > 180:
+        print("Wow surprising")
+    elif x < 20:
+        print("Damn, so small")
+    else:
+        print("quite average")
+
+rate_number(k)
